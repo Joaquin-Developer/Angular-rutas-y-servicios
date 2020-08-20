809f7e33a93e439c2ca22c8fd2cdaa7bbc32f58e
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-08-20 20:59:11 -0300

    creo los 3 componentes que usaré

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { FooterComponent } from './footer/footer.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    FooterComponent,
+    CuerpoComponent
   ],
   imports: [
     BrowserModule,
