5fe831b77522af4ee7949d5f4e164f75ed7b9012
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-08-20 21:03:14 -0300

    agrego dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { FooterComponent } from './footer/footer.component';
 import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { Cuerpo2Component } from './cuerpo2/cuerpo2.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
     FooterComponent,
-    CuerpoComponent
+    CuerpoComponent,
+    Cuerpo2Component,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
