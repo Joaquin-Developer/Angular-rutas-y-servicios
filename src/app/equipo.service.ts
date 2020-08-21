4c09bc12e2eebfd8ceefa667d94df4cced286240
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-08-21 12:50:53 -0300

    Agrego un array de objetos para exportarlo y llamarlo desde el componente

--- src/app/equipo.service.ts
+++ src/app/equipo.service.ts
@@ -5,8 +5,27 @@ import { Injectable } from '@angular/core';
 })
 export class EquipoService {
 
+  equipo:any[] = [
+    {
+      nombre: "Joaquín Parrilla",
+      especialidad: "Java",
+      descripcion: "Desarrolla en Java desde el año 2017. Tambien tiene conocimentos" +
+      " en .NET, KOtlin, y Angular en el frontend."
+    },
+    {
+      nombre: "Maria Perez",
+      especialidad: "C#",
+      descripcion: "Desarrolla con C#, Visual C++ y Visual Basic .NET desde el año 2008. Es una senior .NET Developer"
+    }
+  ];
+
   constructor() { 
     // probamos el servicio
     console.log("Si vemos este mensaje, significa que nuestro servicio fuciona!");
   }
+  // exportamos el array de objetos creado arriba-
+  obtenerEquipo() {
+    return this.equipo;
+  }
+
 }
